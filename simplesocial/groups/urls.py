e70bf0ff93a16643b55f903fb74395b5925a8dbe
mardzien <mardzien@gmail.com>
2020-03-19 23:13:33 +0100

    connecting group views

---
+++ simplesocial/groups/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'groups'
+
+urlpatterns = [
+    path('', views.ListGroup.as_view(), name='all'),
+    path('new/', views.CreateGroup.as_view(), name='create'),
+    path("posts/in/<slug>/",views.SingleGroup.as_view(),name="single"),
+    
+]
